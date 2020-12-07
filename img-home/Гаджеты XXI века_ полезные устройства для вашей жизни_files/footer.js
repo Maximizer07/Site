e2666084da43cc16c8038fbd3a5005701a3c5173
Maxim <maxim_zykov@inbox.ru>
2020-12-07 14:34:22 +0300

    DD

--- img-home/Гаджеты XXI века_ полезные устройства для вашей жизни_files/footer.js
+++
@@ -1,8 +0,0 @@
-$(function() { $('#footer').load('footer.html') })
-function ValidMail() {
-    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-    var myMail = document.getElementById('input-email').value;
-    var valid = re.test(myMail);
-    if (valid) alert('Вы успешно подписались на рассылку!');
-    else alert('Адрес электронной почты введен неправильно!');
-}
